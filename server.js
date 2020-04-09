124694410d3e5fb0aa81520a87992cb2c4b6928c
Modern Viking <thomascustoms715@gmail.com>
2020-04-09 11:13:33 -0600

    changing file path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, './build');
 app.use(express.static(publicPath));
 
-require("./routes/htmlRoutes")(app);
+require("./Backend/routes/htmlRoutes")(app);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
